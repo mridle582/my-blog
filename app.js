c0120125d8243ffcdcfb489c1517d70cc13895be
mridle582 <mridle582@gmail.com>
2022-07-27 09:35:48 -0400

    add function to navbar pages

--- app.js
+++ app.js
@@ -21,6 +21,17 @@ app.get("/", (req, res) => {
   });
 });
 
+app.get("/about", (req, res) => {
+  res.render("about", {
+    aboutText: aboutContent
+  });
+});
+
+app.get("/contact", (req, res) => {
+  res.render("contact", {
+    contactText: contactContent
+  });
+});
 
 
 
