481ff5c6fbf468df44e91eb051f2e014129feb7f
mridle582 <mridle582@gmail.com>
2022-08-03 10:55:09 -0400

    remove unneeded node packages

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const _ = require("lodash");
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -59,13 +58,11 @@ app.get("/compose", (req, res) => {
 });
 
 app.get("/posts/:postId", (req, res) => {
-    console.log(req.params);
     const qPostId = req.params.postId;
 
     Post.findOne({
         _id: qPostId
     }, (err, qPost) => {
-        console.log(qPost);
         if (err) {
             console.log(err);
         } else {
@@ -74,7 +71,7 @@ app.get("/posts/:postId", (req, res) => {
                     postTitle: "Ooops!",
                     postBody: "Could not find the desired page, please try again."
                 });
-            }else {
+            } else {
                 res.render("post", {
                     postTitle: qPost.title,
                     postBody: qPost.body
@@ -94,6 +91,6 @@ app.post("/compose", (req, res) => {
     });
 });
 
-app.listen(3000,  () => {
+app.listen(3000, () => {
     console.log("Server started on port 3000");
 });
